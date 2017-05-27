53fd678b23dbb1bc026b92dcbf1bfdb13490c92a
gonghf95 <gonghf95@gmail.com>
2017-05-28 00:41:58 +0800

    UserMapper测试完成

--- src/test/java/mapper/UserMapperUnit.java
+++ src/test/java/mapper/UserMapperUnit.java
@@ -5,7 +5,6 @@ import cn.sumi.mapper.UserMapper;
 import cn.sumi.pojo.User;
 import org.junit.After;
 import org.junit.Assert;
-import org.junit.Before;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
@@ -13,37 +12,46 @@ import org.springframework.beans.factory.annotation.Autowired;
  * UserMapper Test Unit
  * Created by gonghf95 on 5/25/17.
  */
-public class UserMapperUnit extends BaseConfiguration{
+public class UserMapperUnit extends BaseConfiguration {
 
     @Autowired
     private UserMapper userMapper;
 
-    @Before
-    public void before() {
-        System.out.println("before");
-    }
-
     @Test
-    public void find(){
+    public void selectByPrimaryKey() {
+        User user1 = userMapper.selectByPrimaryKey("gonghf95");
+        Assert.assertNull(user1);
 
     }
 
+
     @Test
-    public void save(){
-        //User target = new User("admin","123");
-        //userMapper.save(target);
+    public void insert() {
+        User user = new User();
+        user.setAccount("gonghf95");
+        user.setPasswd("123");
+        userMapper.insert(user);
     }
 
     @Test
-    public void update(){
-        //User target = new User("gonghf95","456");
-        //userMapper.update(target);
+    public void deleteByPrimaryKey() {
+        userMapper.deleteByPrimaryKey("gonghf95");
     }
 
     @Test
-    public void delete(){
-        //User user = new User("gonghf95");
-        //userMapper.delete(user);
+    public void updateByPrimaryKeySelective() {
+        User target = new User();
+        target.setAccount("gonghf95");
+        target.setPasswd("888");
+        userMapper.updateByPrimaryKeySelective(target);
+    }
+
+    @Test
+    public void updateByPrimaryKey() {
+        User user = new User();
+        user.setAccount("gonghf95");
+        user.setEmail("gonghf95@gmail.com");
+        userMapper.updateByPrimaryKey(user);
     }
 
     @After
